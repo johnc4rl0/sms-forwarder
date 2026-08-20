c4b6d3129726b20479864db7a78ad59f322d2632
johnc4rl0 <17008814+johnc4rl0@users.noreply.github.com>
2026-08-20 10:57:42 -0500

    Bump release version to 1.0.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,8 +51,8 @@ android {
         applicationId = "com.johnc4rl0.smsforwarder"
         minSdk = 31
         targetSdk = 37
-        versionCode = 2
-        versionName = "1.0.1"
+        versionCode = 3
+        versionName = "1.0.2"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
